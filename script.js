0b0f1543b6b347f79be0a9fb2d8cb0a605f2cb4f
justinwu1 <justinwu820@gmail.com>
2021-01-07 16:28:41 -0500

    Buttons and Large elements

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const password = document.querySelector("#password");
 const form = document.querySelector("form");
 const form2 = document.querySelector("#form2");
+const buttons = document.querySelectorAll("button");
+const button1 = buttons[0];
+const button2 = buttons[1];
+const large = document.querySelector("#large");
+// Question#1
+const h5 = document.querySelector("h5");
+button1.addEventListener("click", function () {
+    h5.innerText = "I'm right";
+})
+button2.addEventListener("click", function () {
+    h5.innerText = "No,I'm right";
+})
+
+large.addEventListener("mouseover", function () {
+    alert("DON'T HOVER OVER ME!");
+})
+console.log(large)
 // Question#5
 form.addEventListener("submit", function (e) {
     e.preventDefault();
@@ -16,6 +33,6 @@ form.addEventListener("submit", function (e) {
 form2.addEventListener("submit", function (e) {
     e.preventDefault();
     const val = form2.getElementsByTagName("input")[0].value;
-    let volume = (4/3) * Math.PI * Math.pow(val, 3);
+    let volume = (4 / 3) * Math.PI * Math.pow(val, 3);
     alert(volume);
 })
